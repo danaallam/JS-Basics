2bf5157214f583a96ce54186fdbab900bc70d519
Dana Allam <dana.allam99@hotmail.com>
2021-05-30 23:19:00 +0300

    Step 1 done

---
+++ javascript-basics-03/step 1/Step_1.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', init);
+function init(){
+    var img=document.querySelector("#image1");
+    img.onmouseover = () => {
+        img.style.border="3px solid red";
+    };
+    img.onmouseout = () => {
+        img.style.border="none";
+    };
+}
